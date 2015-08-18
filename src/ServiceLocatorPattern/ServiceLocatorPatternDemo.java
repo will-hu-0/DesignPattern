ace9722c80e0238b3bcc7af02a012c5fc0747e0f
Will <will@Wills-iMac.local>
2015-08-18 20:19:56 +0800

    Initial commit

---
+++ src/ServiceLocatorPattern/ServiceLocatorPatternDemo.java
@@ -0,0 +1,17 @@
+package ServiceLocatorPattern;
+
+/**
+ * Created by will on 8/18/15.
+ */
+public class ServiceLocatorPatternDemo {
+    public static void main(String[] args) {
+        IService service = ServiceLocator.getService("Service1");
+        service.execute();
+        service = ServiceLocator.getService("Service2");
+        service.execute();
+        service = ServiceLocator.getService("Service1");
+        service.execute();
+        service = ServiceLocator.getService("Service2");
+        service.execute();
+    }
+}
